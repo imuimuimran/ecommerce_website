5202c3fb04fcc7126bcfd3af3caf92b7b381126f
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-06-18 13:51:19 +0600

    Create a home.html page under 'ecom_app/templates/ecom_app' and extend it with a base.html page.

--- ecom/ecom_app/views.py
+++ ecom/ecom_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "ecom_app/base.html")
+    return render(request, "ecom_app/home.html")
