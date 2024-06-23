992881529253736a66cfa4448a0792a39c7c96c2
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-06-23 18:28:09 +0600

    create products tables into the admin panel and modify it also create a class named ProductModelAdmi, which is inherited to admin.ModelAdmin. Also display required things and import products from the same directory.

--- ecom/ecom_app/admin.py
+++ ecom/ecom_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import products
 
 # Register your models here.
+@admin.register(products)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'discounted_price', 'category', 'product_img']
