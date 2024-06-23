100c143fc0e678e80bf72aad8ec6aef4f69397bf
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-06-23 15:27:05 +0600

    in models.py file we create a model for interacting products as required

--- ecom/ecom_app/models.py
+++ ecom/ecom_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = (
+    ('CR', 'Crud'),
+    ('ML', 'Milk'),
+    ('Ls', 'Lassi'),
+    ('MS', 'Milkshake'),
+    ('PN', 'Paneer'),
+    ('GH', 'Ghee'),
+    ('CZ', 'Cheese'),
+    ('IC', 'Ice-Creams'),
+)
+
+class products(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    product_app = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_img = models.ImageField(upload_to='product')
+    
+    def __str__(self):
+        return self.title
+    
