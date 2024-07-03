769ab0b880d98ea2dfd8b456345f68c29ab0fb9c
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-07-03 18:11:58 +0600

    in forms.py Customer model is set with fields and widget and in views.py form is set as CustomerProfileForm in ProfileView

--- ecom/ecom_app/forms.py
+++ ecom/ecom_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm
 from django.contrib.auth.models import User
+from . models import Customer
 
 class LoginForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus ': 'True', 'class ': 'form-control'}))
@@ -20,4 +21,14 @@ class MyPasswordResetForm(PasswordChangeForm):
     pass
 
 class CustomerProfileForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Customer
+        fields = ['name', 'locality', 'city', 'mobile', 'division', 'zipcode']
+        widgets={
+            'name' : forms.TextInput(attrs={'class ': 'form-control'}),
+            'locality' : forms.TextInput(attrs={'class ': 'form-control'}),
+            'city' :  forms.TextInput(attrs={'class ': 'form-control'}),
+            'mobile' : forms.NumberInput(attrs={'class ': 'form-control'}),
+            'division' : forms.Select(attrs={'class ': 'form-control'}),
+            'zipcode' : forms.NumberInput(attrs={'class ': 'form-control'}),
+        }
--- ecom/ecom_app/views.py
+++ ecom/ecom_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from . models import products
 from django.db.models import Count
-from . forms import CustomerRegistrationForm
+from . forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 
 # Create your views here.
@@ -50,6 +50,7 @@ class CustomerRegistrationView(View):
 class ProfileView(View):
     
     def get(self, request):
+        form = CustomerProfileForm()
         return render(request, "ecom_app/profile.html", locals())
     
     def post(self, request):
