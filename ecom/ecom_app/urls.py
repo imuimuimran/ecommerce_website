a00b2affccf74b867bb45b6f4b86db2ad5f1ed73
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-07-09 16:32:12 +0600

    create four html files which are just added in urls.py file: password_reset.html, password_reset_done.html, password_reset_confirm.html and password_reset_complete.html

--- ecom/ecom_app/urls.py
+++ ecom/ecom_app/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     
     path("password-reset/", auth_view.PasswordResetView.as_view(template_name="ecom_app/password_reset.html", form_class=MyPasswordResetForm), name="password_reset"),
     path("password-reset/done/", auth_view.PasswordResetDoneView.as_view(template_name="ecom_app/password_reset_done.html"), name="password_reset_done"),
-    path("password-reset-confirm/<uidb64>/<token>", auth_view.PasswordResetConfirmView.as_view(template_name="ecom_app/password_reset_confirm.html", form_class=MySetPasswordForm), name="password_reset_confirm"),
+    path("password-reset-confirm/<uidb64>/<token>/", auth_view.PasswordResetConfirmView.as_view(template_name="ecom_app/password_reset_confirm.html", form_class=MySetPasswordForm), name="password_reset_confirm"),
     path("password-reset-complete/", auth_view.PasswordResetCompleteView.as_view(template_name="ecom_app/password_reset_complete.html"), name="password_reset_complete"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
