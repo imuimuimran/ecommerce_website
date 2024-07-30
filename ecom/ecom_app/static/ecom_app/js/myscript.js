57ca28cc8cd617eeaee28f9991802b081ddfb56c
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-07-31 02:41:39 +0600

    like 'pluscart' in cart section 'minuscart' and 'removecart' are added. in views.py file 'minus_cart' and 'remove_cart' is define and in urls.py file directs them as path.

--- ecom/ecom_app/static/ecom_app/js/myscript.js
+++ ecom/ecom_app/static/ecom_app/js/myscript.js
@@ -16,3 +16,39 @@ $('.plus-cart').click(function(){
         }
     })
 })
+
+$('.minus-cart').click(function(){
+    var id = $(this).attr("pid").toString();
+    var em1 = this.parentNode.children[2]
+    console.log("pid = ", id)
+    $.ajax({
+        type:"GET",
+        url:"/minuscart",
+        data:{
+            prod_id:id
+        },
+        success:function(data){
+            console.log("data= ",data);
+            em1.innerText=data.quantity
+            document.getElementById("amount").innerText=data.amount
+            document.getElementById("totalamount").innerText=data.totalamount
+        }
+    })
+})
+
+$('.remove-cart').click(function(){
+    var id = $(this).attr("pid").toString();
+    var em1 = this
+    $.ajax({
+        type:"GET",
+        url:"/removecart",
+        data:{
+            prod_id:id
+        },
+        success:function(data){
+            document.getElementById("amount").innerText=data.amount
+            document.getElementById("totalamount").innerText=data.totalamount
+            em1.parentNode.parentNode.parentNode.parentNode.remove()
+        }
+    })
+})
