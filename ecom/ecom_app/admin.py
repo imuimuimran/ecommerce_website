c83e0894dcf8743201d2edf7c04d0feab9d6c3c6
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-08-15 22:15:42 +0600

    randome changes

--- ecom/ecom_app/admin.py
+++ ecom/ecom_app/admin.py
@@ -19,5 +19,5 @@ class PaymentModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'amount', 'razorpay_order_id', 'razorpay_payment_status', 'razorpay_payment_id', 'paid']
     
 @admin.register(OrderPlaced)
-class PaymentModelAdmin(admin.ModelAdmin):
+class OrderPlacedModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'customer', 'product', 'quantity', 'ordered_date', 'status', 'payment']
